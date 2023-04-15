347f6e3ce3209f77ff42996fe57726a6fc0ca52a
YuriiBurlachenko <burlachenkoyurii@gmail.com>
2023-04-15 22:37:36 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let num1 = Number(
+  prompt('This is converting hours to seconds, please enter hours:')
+);
+let result = `${num1 * 3600}`;
+alert(`${num1 + ' ' + 'hours have seconds:' + '  ' + result}`);
